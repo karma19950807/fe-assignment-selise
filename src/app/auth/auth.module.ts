6939792b52f16b9c23233b094365926e6217b03a
karma19950807 <kasamdrup1995@gmail.com>
2021-10-25 16:54:25 +0600

    designing signin and signup page

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,10 +4,34 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [SigninComponent, SignupComponent],
-  imports: [CommonModule, AuthRoutingModule],
-  exports: [SigninComponent, SignupComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
+  exports: [
+    SigninComponent,
+    SignupComponent,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatToolbarModule,
+  ],
 })
 export class AuthModule {}
